ded9db7be68dc2c45483915aed16058dd878325b
Kalliduz <kalliduz.dev@gmail.com>
2023-04-13 19:04:20 +0200

    moved agents to subfolder

--- AutoGPT.dpr
+++ AutoGPT.dpr
@@ -2,21 +2,22 @@
 
 uses
   Vcl.Forms,
-  Main in 'Main.pas' {Form1},
-  Agent in 'Agent.pas',
-  Agent.GPT in 'Agent.GPT.pas',
+  Main in 'Main.pas' {frmAutoGPTGUI},
   AutoGPT.Manager in 'AutoGPT.Manager.pas',
-  Agent.ReadFile in 'Agent.ReadFile.pas',
-  Agent.WriteFile in 'Agent.WriteFile.pas',
-  Agent.Browse in 'Agent.Browse.pas',
-  Agent.GoogleSearch in 'Agent.GoogleSearch.pas',
-  Agent.User in 'Agent.User.pas',
-  Agent.Memory in 'Agent.Memory.pas',
-  Agent.ListFiles in 'Agent.ListFiles.pas',
   Logging in 'Logging.pas',
-  Agent.CMD in 'Agent.CMD.pas',
   Vcl.Themes,
-  Vcl.Styles;
+  Vcl.Styles,
+  AutoGPT.Options in 'AutoGPT.Options.pas',
+  Agent.Browse in 'Agents\Agent.Browse.pas',
+  Agent.CMD in 'Agents\Agent.CMD.pas',
+  Agent.GoogleSearch in 'Agents\Agent.GoogleSearch.pas',
+  Agent.GPT in 'Agents\Agent.GPT.pas',
+  Agent.ListFiles in 'Agents\Agent.ListFiles.pas',
+  Agent.Memory in 'Agents\Agent.Memory.pas',
+  Agent in 'Agents\Agent.pas',
+  Agent.ReadFile in 'Agents\Agent.ReadFile.pas',
+  Agent.User in 'Agents\Agent.User.pas',
+  Agent.WriteFile in 'Agents\Agent.WriteFile.pas';
 
 {$R *.res}
 
@@ -24,6 +25,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   TStyleManager.TrySetStyle('Tablet Dark');
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TfrmAutoGPTGUI, frmAutoGPTGUI);
   Application.Run;
 end.
